90cc1e55123ad9f0606588b43833d11276a301ed
ernane <ernanecarvalho89@gmail.com>
2022-03-05 11:34:04 -0300

    Demonstrando o uso de useState

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handlePlusClick = () => {
+    if(count < 10) {
+      setCount(count + 1);
+    }
+  }
+
+  const handleMinusClick = () => {
+    if(count > 0){
+      setCount(count -1);
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Contador: {count}</h1>
+      <button class="plus" onClick={handlePlusClick}>+</button>
+      <button class="minus" onClick={handleMinusClick}>-</button>
+    </>
   );
 }
 
